4655604f8712cdf48dcff40446b3c7d1c188b0ff
Your Name <fst.o.aissa21@fst.univ-dbkm.dz>
2025-03-03 17:03:13 +0100

    add note states

--- lib/cubits/add_note_cubit/add_note_cubits_state.dart
+++ lib/cubits/add_note_cubit/add_note_cubits_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubits_cubit.dart';
 sealed class AddNoteCubitsState {}
 
 final class AddNoteCubitsInitial extends AddNoteCubitsState {}
+
+final class AddNoteCubitsLoading extends AddNoteCubitsState {}
+
+final class AddNoteCubitsFailure extends AddNoteCubitsState {
+  final String errorMassage;
+
+  AddNoteCubitsFailure(this.errorMassage);
+}
+
+final class AddNoteCubitsSuccess extends AddNoteCubitsState {}
