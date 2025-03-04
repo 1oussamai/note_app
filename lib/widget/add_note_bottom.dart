18693dab6f6a95818c8a92e0dee302b64cde1c92
Your Name <fst.o.aissa21@fst.univ-dbkm.dz>
2025-03-04 16:39:49 +0100

    adding blockConsumer and handle states

--- lib/widget/add_note_bottom.dart
+++ lib/widget/add_note_bottom.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubits_cubit.dart';
 import 'package:note_app/widget/add_note_form.dart';
-
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.all(17),
-        child: AddNoteForm(),
+        padding: const EdgeInsets.all(17),
+        child: BlocConsumer<AddNoteCubit, AddNoteCubitsState>(
+          listener: (context, state) {
+            if (state is AddNoteCubitsFailure) {
+              print('object');
+            }
+            if (state is AddNoteCubitsSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteCubitsLoading ? true : false,
+              child: const AddNoteForm());
+          },
+        ),
       ),
     );
   }
 }
-
